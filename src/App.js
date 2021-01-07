357500b3c6bc986aeaf6fadf45b2411923811121
alexthotse <alexthotse@gmail.com>
2021-01-07 02:39:03 +0200

    Created a Todo app with React based off a DigitalOcean Tutorial.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,101 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+	  <div 
+		  className="todo"
+		  style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+		>
+		  {todo.text}
+		  <div>
+		    <button onClick={() => completeTodo(index)}>Completed</button>
+		    <button onClick={() => removeTodo(index)}>[X]</button>
+		  </div>
+		</div>
+	);
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+	const handleSubmit = e => {
+	  e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		//setvalue("");
+	};
+
+	return (
+	  <form onSubmit={handleSubmit}>
+			<input
+		    type="text"
+		    className="input"
+		    value={value}
+		    onChange={e => setValue(e.target.value)}
+		  />
+		</form>
+	);
+}
+
+function App() {
+  const [todos, setTodos] = React.useState([
+	  { 
+			text: "Learn about React",
+		  isCompleted: false
+		},
+		{ 
+			text: "Meet friend for lunch",
+		  isCompleted: false
+		},
+		{ 
+			text: "Build really cool todo app",
+		  isCompleted: false
+		}	
+	]);
+ 
+	const addTodo = text => {
+	  const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+ 
+	const completeTodo = index => {
+	  const newTodos = [...todos];
+		newTodos[index].isCompleted = true;
+		setTodos(newTodos);
+	};
+
+  const removeTodo = index => {
+	  const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return(
+		/*<h1>I love my life</h1>*/
+		<div className="app">
+		  <div className="todo-list">
+		    {todos.map((todo, index) => (
+				  <Todo
+					  key={index}
+					  index={index}
+					  todo={todo}
+					  completeTodo={completeTodo}
+					  removeTodo={removeTodo}
+					/>
+				))}
+		    <TodoForm addTodo={addTodo} />
+		  </div>
+		</div>
+	);
+
+}
+
+export default App;
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -23,3 +118,4 @@ function App() {
 }
 
 export default App;
+*/
